07024ec7bcda8514a2b493ef03c25faf38c68180
setanan <52027005+setanan@users.noreply.github.com>
2019-09-12 13:32:33 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1568268158,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite9-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"images/sprite11-sheet0.png",
+		"images/sprite12-sheet0.png",
+		"images/tree-sheet0.png",
+		"images/tree-sheet1.png",
+		"images/tree-sheet2.png",
+		"images/sprite14-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
